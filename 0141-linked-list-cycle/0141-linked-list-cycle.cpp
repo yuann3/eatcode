5e119d714bc8fa6d57f3a0348fc351fa6085488b
Yiyuan Li <etherealdt@gmail.com>
2024-08-16 16:19:47 +0800

    Time: 3 ms (99.09%), Space: 10.9 MB (47.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,14 +12,13 @@ public:
        if (head == NULL) return false;
 
        ListNode* slow = head;
-       ListNode* fast = head;     
+       ListNode* fast = head; 
 
        while (fast->next != NULL && fast->next->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
-        if (slow == fast) {
-            return true;
-        }
+
+        if (slow == fast) return true;
        }
        return false;
     }
