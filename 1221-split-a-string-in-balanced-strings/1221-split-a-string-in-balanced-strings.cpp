a8f90525d1a723dd0efa39f04c5cbbf9a53558e7
Yiyuan Li <etherealdt@gmail.com>
2024-11-18 20:03:32 +0800

    Time: 0 ms (100%), Space: 7.6 MB (82.08%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0;
+        int balance = 0;
+
+        for (char c : s) {
+            if (c == 'R') {
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
