bd5f2aa3b022dce764dc027fa426981a8f3b56a4
Yiyuan Li <etherealdt@gmail.com>
2024-12-28 22:27:51 +0800

    Time: 0 ms (100%), Space: 9.9 MB (51.5%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<vector<int>> res = {{}};
+
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                vector<int> subset = res[i];
+                subset.push_back(num);
+                res.push_back(subset);
+            }
+        }
+
+        return res; 
+    }
+};
