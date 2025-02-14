b7a2850ff8b49c9c0039d5633f5b974e1d21baa4
Yiyuan Li <etherealdt@gmail.com>
2025-02-14 16:04:10 +0800

    Time: 0 ms (100%), Space: 17.6 MB (78.77%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,14 +5,15 @@ class Solution:
 
         left = 1
         right = x
-        
+
         while left <= right:
-            mid = (left + right) // 2
+            mid = (right + left) // 2
+
             if mid * mid == x:
                 return mid
             elif mid * mid < x:
                 left = mid + 1
             else:
                 right = mid - 1
-        
+
         return right
